c9d76e5cbe0cb6610a7440343527928454154f50
Rahul Kumar <rahul@thoughtnirvana.com>
2019-01-04 02:40:30 +0530

    Game over conditions

--- core/src/com/mygdx/game/spacerocks/LevelScreen.java
+++ core/src/com/mygdx/game/spacerocks/LevelScreen.java
@@ -1,9 +1,11 @@
 package com.mygdx.game.spacerocks;
 
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 
 public class LevelScreen extends BaseScreen {
     private Spaceship spaceship;
+    private boolean gameOver = false;
 
     public void initialize() {
         BaseActor space = new BaseActor(0, 0, mainStage);
@@ -24,13 +26,21 @@ public class LevelScreen extends BaseScreen {
     }
 
     public void update(float dt) {
-        for (BaseActor rockActor : BaseActor.getList(mainStage, "Rock")) {
+        for (BaseActor rockActor : BaseActor.getList(mainStage, "com.mygdx.game.spacerocks.Rock")) {
             if (rockActor.overlaps(spaceship)) {
                 Explosion boom = new Explosion(0, 0, mainStage);
                 if (spaceship.shieldPower <= 0) {
                     boom.centerAtActor(spaceship);
                     spaceship.remove();
                     spaceship.setPosition(-1000, -1000);
+
+                    BaseActor messageLose = new BaseActor(0, 0, uiStage);
+                    messageLose.loadTexture("message-lose.png");
+                    messageLose.centerAtPosition(400, 300);
+                    messageLose.setOpacity(0);
+                    messageLose.addAction(Actions.fadeIn(1));
+                    gameOver = true;
+
                 } else {
                     spaceship.shieldPower -= 34;
                     boom.centerAtActor(rockActor);
@@ -38,13 +48,22 @@ public class LevelScreen extends BaseScreen {
                 }
             }
 
-            for (BaseActor laserActor : BaseActor.getList(mainStage, "Laser")) {
+            for (BaseActor laserActor : BaseActor.getList(mainStage, "com.mygdx.game.spacerocks.Laser")) {
                 if (laserActor.overlaps(rockActor)) {
                     Explosion boom = new Explosion(0, 0, mainStage);
                     boom.centerAtActor(rockActor);
                     laserActor.remove();
                     rockActor.remove();
                 }
+            }
+
+            if (!gameOver && BaseActor.count(mainStage, "com.mygdx.game.spacerocks.Rock") == 0) {
+                BaseActor messageWin = new BaseActor(0, 0, uiStage);
+                messageWin.loadTexture("message-win.png");
+                messageWin.centerAtPosition(400, 300);
+                messageWin.setOpacity(0);
+                messageWin.addAction(Actions.fadeIn(1));
+                gameOver = true;
             }
         }
     }
